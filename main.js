32ac88368147bd821d53e09ac43f309906bd54e2
Vibhab-T <vibhabtimsina86@gmail.com>
2024-06-11 19:28:19 +0545

    added a subscription box modal

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 let menu = document.querySelector(".menuOpen");
 let close = document.querySelector(".menuClose");
+let sub = document.querySelector(".sub");
+let sub2 = document.querySelector("#sub");
+let card = document.querySelector(".pricing-bg");
 
 menu.addEventListener("click", () => {
   let sideBar = document.querySelector(".sidebar");
@@ -9,4 +12,14 @@ menu.addEventListener("click", () => {
 close.addEventListener("click", () => {
   let sideBar = document.querySelector(".sidebar");
   sideBar.style.display = "none";
+});
+
+sub.addEventListener("click", () => {
+  console.log("helo");
+  card.classList.toggle("active");
+});
+
+sub2.addEventListener("click", () => {
+  console.log("helo");
+  card.classList.toggle("active");
 });
